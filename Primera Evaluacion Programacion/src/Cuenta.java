2156b99ce5b5ea0fd993f2a311f8579062c6a6cb
Cesar Rodriguez <cesar.rodriguez8@educa.madrid.org>
2021-02-18 15:52:43 +0100

    16/02/2021

---
+++ Primera Evaluacion Programacion/src/Cuenta.java
@@ -0,0 +1,10 @@
+public class Cuenta {
+
+    private String titular;
+    private long numero;
+    private long saldo;
+    private boolean bloqueada;
+    public Cuenta(String titular, long numero){
+
+    }
+}
---
+++ Primera Evaluacion Programacion/src/EjerciciosConstantes.java
@@ -0,0 +1,28 @@
+public class EjerciciosConstantes {
+
+    public static final int DIA_DE_LA_SEMANA = 7;
+    public static final int MES_DEL_AÑO = 12;
+    public static final int HORAS_DEL_DIA = 24;
+
+
+    public static int CalculaHoras (int a){
+        int resultadoCalculaHoras = DIA_DE_LA_SEMANA * 24;
+        return resultadoCalculaHoras;
+    }
+    public static int CalcularSemanas (int a, int b){
+
+        int semanas = a * 7;
+        int dias = b;
+        int resultadoCalcularSemanas = semanas + dias;
+        return resultadoCalcularSemanas;
+    }
+    public static int CalcularAños (int a, int b){
+        int resultadoCalcularAños =
+    }
+    public static int calcularAreaCírculo (long a, int b){
+        int resultadocalcularAreaCírculo =
+    }
+    public static int calcularVolumenCírculo (long a){
+        int resultadocalcularVolumenCírculo =
+    }
+}
